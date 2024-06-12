e692a1346ec9b4b90e3fa081626d11a6beedaef1
AlbertoCavans <alberto96cavani@gmail.com>
2024-06-12 16:52:34 +0200

    Feat - snack - pick_only_even_numbers

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const number = document.querySelector(".numbers");
 
 for (let i = 0; i < 100; i++) {
   if (i % 2 == 0) {
-    number.innerHTML += `<li class="number text-danger">Stampiamo n.o: ${i}</li>`;
-  } else {
-    number.innerHTML += `<li class="number text-primary">Stampiamo n.o: ${i}</li>`;
+    number.innerHTML += `<li class="number">Stampiamo n.o: ${i}</li>`;
   }
 }
