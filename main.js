9e51bba9f76332e5aa30495c87a0c933b98d14b4
AlbertoCavans <alberto96cavani@gmail.com>
2024-06-12 16:51:37 +0200

    Feat - snack - pick_odd_or_even_numbers

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const number = document.querySelector(".numbers");
 
-for (let i = 100; i > 0; i--) {
-  number.innerHTML += `<li class="number">Stampiamo n.o: ${i}</li>`;
+for (let i = 0; i < 100; i++) {
+  if (i % 2 == 0) {
+    number.innerHTML += `<li class="number text-danger">Stampiamo n.o: ${i}</li>`;
+  } else {
+    number.innerHTML += `<li class="number text-primary">Stampiamo n.o: ${i}</li>`;
+  }
 }
