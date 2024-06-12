0bfa7367a60ab50ad5bca1b3091cbd39bd2d89f2
AlbertoCavans <alberto96cavani@gmail.com>
2024-06-12 16:44:09 +0200

    Feat - Snack  - for_loop_numbers_from_100_to_1

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-let userName = document.getElementById("user-name");
-let userGender = document.getElementById("user-gender");
-let button = document.getElementById("button");
-let result = document.getElementById("result");
+const number = document.querySelector(".numbers");
 
-button.addEventListener("click", function () {
-  if (userGender.value == "maschio") {
-    result.className = "text-primary";
-    console.log(result.className);
-    result.innerText = "Il mio nome è " + userName.value;
-  } else if (userGender.value == "femmina") {
-    result.className = "text-danger";
-    console.log(result.className);
-    result.innerText = "Il mio nome è " + userName.value;
-  } else {
-    result.className = "text-success";
-    console.log(result.className);
-    result.innerText = "Il mio nome è " + userName.value;
-  }
-});
+for (let i = 100; i > 0; i--) {
+  number.innerHTML += `<li class="number">Stampiamo n.o: ${i}</li>`;
+}
