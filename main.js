5f40ae780e113d332329f04c672885a7dc6e3700
AlbertoCavans <alberto96cavani@gmail.com>
2024-06-13 16:58:27 +0200

    Feat - print even_numbers of an array using a for_loop

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const number = document.querySelector(".numbers");
+const numbers = document.querySelector(".numbers");
+const array = [2, 5, 18, 45, 8, 6];
 
-for (let i = 0; i < 100; i++) {
-  if (i % 2 == 0) {
-    number.innerHTML += `<div class="col-2"><div class="card my-2">Stampiamo n.o: ${i}</div></div>`;
+// numbers.innerHTML = `<div class="col-2">`;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 == 0) {
+    numbers.innerHTML += `<div class="col-2 card m-2">Stampiamo n.o: ${array[i]}</div>`;
   }
 }
+// numbers.innerHTML += `</div>`;
+// `<div class="col-2"><div class="card my-2">Stampiamo n.o: ${i}</div></div>`
