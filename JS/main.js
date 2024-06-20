17a995284c0f04e68dba9ab57b3365f27ce944f0
AlbertoCavans <alberto96cavani@gmail.com>
2024-06-20 18:29:13 +0200

    Feat - Snack print invited user

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,21 @@
-// Const-Elements
-let totNumber = document.querySelector(".text-primary");
-let arrayNumbers = [];
-let partialTot = 0;
+const secretList = ["Mike", "Teto", "Nico", "Giada", "Rovvi", "Albe"];
+const result = document.querySelector(".text-primary");
+let userName = prompt("Scrivi il tuo nome");
+let userInvited = false;
 
-while (arrayNumbers.length < 10) {
-  let factorNumber = parseInt(prompt("Scrivi un numero"));
+for (let i = 0; i < secretList.length; i++) {
+  const listMember = secretList[i];
 
-  console.log(factorNumber);
-  console.log(typeof factorNumber);
-  partialTot += factorNumber;
-  console.log(partialTot);
-  arrayNumbers.push(factorNumber);
-  console.log(arrayNumbers);
-  totNumber.innerHTML += `<li class="text-primary">${factorNumber}</li>`;
+  if (listMember == userName) {
+    console.log(userName);
+    console.log(listMember);
+    userInvited = true;
+    console.log(userInvited);
+  }
+
+  if (userInvited) {
+    result.innerText = "Fai parte della lista";
+  } else {
+    result.innerText = "Non fai parte della lista";
+  }
 }
-
-totNumber.innerHTML += `<li class="text-danger">Tot: ${partialTot}</li>`;
