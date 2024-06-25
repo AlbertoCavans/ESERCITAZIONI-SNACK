e728e379fe435e38aa38f4e8a2d203da4b10447b
AlbertoCavans <alberto96cavani@gmail.com>
2024-06-25 18:51:08 +0200

    Feat - functions get random number between max and min

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,55 @@
 const randomizerButton = document.querySelector(".btn-primary");
+const minNumber = document.getElementById("min");
+const maxNumber = document.getElementById("max");
+let maxNumb = 0;
+let minNumb = 0;
 
-function getRandomNumber() {
+function newMaxNumbs() {
+  maxNumb = parseInt(prompt("Scrivi il numero maggiore del range desiderato"));
+  if (isNaN(maxNumb)) {
+    alert("Devi inserire un numero!");
+    maxNumb = parseInt(
+      prompt("Scrivi il numero maggiore del range desiderato")
+    );
+  } else {
+    console.log(maxNumb);
+    return maxNumb;
+  }
+}
+
+function newMinNumbs() {
+  minNumb = parseInt(prompt("Scrivi il numero minore del range desiderato"));
+  if (isNaN(minNumb)) {
+    alert("Devi inserire un numero!");
+    minNumb = parseInt(prompt("Scrivi il numero minore del range desiderato"));
+  } else if (minNumb > maxNumb) {
+    alert("Devi inserire un numero minore rispetto al maggiore del range!");
+    minNumb = parseInt(prompt("Scrivi il numero minore del range desiderato"));
+  } else {
+    console.log(minNumb);
+    return minNumb;
+  }
+}
+
+newMaxNumbs();
+newMinNumbs();
+
+minNumber.innerText = minNumb;
+maxNumber.innerText = maxNumb;
+
+console.log(maxNumb);
+console.log(minNumb);
+
+function getRandomNumber(max, min) {
   const printedNumber = document.querySelector(".text-primary");
-
-  let numb = Math.floor(Math.random() * 100);
+  console.log(maxNumb);
+  console.log(minNumb);
+  let numb = Math.floor(Math.random() * max) + min;
   console.log(numb);
   printedNumber.innerText = numb;
   return;
 }
 
-randomizerButton.addEventListener("click", getRandomNumber);
+randomizerButton.addEventListener("click", function () {
+  getRandomNumber(maxNumb, minNumb);
+});
