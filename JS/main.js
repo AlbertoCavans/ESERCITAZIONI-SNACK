c1a2191f70980bf2ff1d9d463d5f15332dcbc3db
AlbertoCavans <alberto96cavani@gmail.com>
2024-07-04 11:14:14 +0200

    Feat - sum calculator

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
-const myArray = [2, 5, 6, 7, 8, 8, 5, 4, 3, 2, 1, 5];
+// ! Counter number Snack
+/* const myArray = [2, 5, 6, 7, 8, 8, 5, 4, 3, 2, 1, 5];
 const myNumber = 5;
 
 const myNumb = document.getElementById("myNumber");
@@ -36,4 +37,49 @@ function countNumber(startArray, countingNumber) {
 
 countButton.addEventListener("click", function () {
   countNumber(myArray, myNumber);
-});
+}); */
+
+// ! Add numbers until the user submit "zero"
+const printResult = document.getElementById("result");
+let printArray = document.getElementById("myArray");
+const userNumbers = [];
+let result = 0;
+let userNumber;
+
+userNumber = parseInt(
+  prompt(
+    'Inserisci un numero diverso da "zero", se invii "zero" otterrai solo lo "zero" stesso'
+  )
+);
+
+while (userNumber != 0) {
+  if (isNaN(userNumber)) {
+    userNumber = parseInt(
+      prompt(
+        'Hai inserito un input che non è un numero! Devi Inserire un numero, verrà sommato ai numeri precedentemente inseriti. Se invii "zero" otterrai la somma, se invii un numero diverso da "zero" puoi inserire dei nuovi numeri da sommare'
+      )
+    );
+  } else {
+    userNumbers.push(userNumber);
+    console.log(userNumbers);
+    result += userNumber;
+    console.log("Somma temporanea = " + result);
+    userNumber = parseInt(
+      prompt(
+        'Inserisci un altro numero, verrà sommato ai numeri precedentemente inseriti. Se invii "zero" otterrai la somma, se invii un numero diverso da "zero" puoi inserire dei nuovi numeri da sommare'
+      )
+    );
+  }
+}
+
+for (let i = 0; i < userNumbers.length; i++) {
+  if (i < userNumbers.length - 1) {
+    printArray.innerText += userNumbers[i] + `-`;
+    console.log(userNumbers[i]);
+  } else {
+    printArray.innerText += `${userNumbers[i]}.`;
+    console.log(userNumbers[i]);
+  }
+}
+
+printResult.innerText = result;
